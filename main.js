0c99931ee25d61ca5240c1a80342e6ecb146129d
PrishaTripathi <83777993+PrishaTripathi@users.noreply.github.com>
2022-12-03 16:51:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+difference = 0;
+rightWristX = 0;
+lefttWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(500, 500);
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.lefttWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX=" + leftWristX + "rightWristX=" + rightWristX + "difference=" + difference);
+    }
+}
+
+function draw() {
+    background('#cc00cc');
+    document.getElementById("font_size").innerHTML = "Font SIze Of The TEXT Will Be = " + difference + "px";
+    textSize(difference);
+    fill('#4d79ff');
+    text('Prisha', 50, 400);
+}
